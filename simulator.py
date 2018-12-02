e8979868c6dc1caa14a2fa0af4af1930b74400ca
Luan Simões <luansimoes@id.uff.br>
2018-12-02 10:58:57 -0200

    Adding method to calculate expected number of elements

--- simulator.py
+++ simulator.py
@@ -9,6 +9,7 @@ class Simulator:
         self.queue_size = queue_size
         self.lamb = lamb
         self.lamb_server = lamb_server
+        self.progression = []
 
     def simulate(self, initial_event, simulation_time):
         self.time = 0
@@ -19,6 +20,8 @@ class Simulator:
         self.num_dropouts = 0
         self.simulation_time = simulation_time
 
+        self.progression.append([len(self.queue), self.time])
+
         while(self.time <= simulation_time):
             if(self.events[0].__class__.__name__ == "Arrival"):
                 self.time = self.events[0].time
@@ -27,6 +30,8 @@ class Simulator:
                     self.queue.append(Element(self.time))
                     if(self.num_arrivals == 1):
                         self.queue[0].start_processing = self.time
+                    else:
+                        self.progression.append([len(self.queue), self.time])
                 else:
                     self.num_dropouts += 1
                 self.events.pop(0)
@@ -41,6 +46,7 @@ class Simulator:
                 self.time = self.events[0].time
                 self.queue[0].exit = self.time
                 self.processed_queue.append(self.queue.pop(0))
+                self.progression.append([len(self.queue), self.time])
                 if(self.queue):
                     self.queue[0].start_processing = self.time
                 self.events.pop(0)
@@ -76,9 +82,25 @@ class Simulator:
         print("-------------------------------------------")
         return (w/self.simulation_time)
 
+
+    def expected_number_of_elements(self):
+        integral = 0
+        for i in range(len(self.progression)-1):
+            delta = self.progression[i+1][1] - self.progression[i][1]
+            integral += self.progression[i][0]*delta
+
+        expected_number = integral/self.simulation_time
+        print(colored("| Número Esperado de Elementos na Fila: " + str(expected_number), "blue"))
+        print("-------------------------------------------")
+        return expected_number
+
+
+
+
 teste = Simulator(15, 60, 20)
 inicial = Arrival(0)
 teste.simulate(inicial, 20)
 teste.average_time()
 teste.dropout_taxe()
 teste.use_taxe()
+teste.expected_number_of_elements()
