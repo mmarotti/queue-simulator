d21a22cf804b4ea8d48811e4bafbf513e8ed9258
PradoTPS <prado.tps@gmail.com>
2018-12-03 21:49:20 -0200

    Started report

--- simulator.py
+++ simulator.py
@@ -3,6 +3,12 @@
 from event import Event, Arrival, Exit
 from element import Element
 from termcolor import colored
+import matplotlib.pyplot as plt
+
+average_time_group = []
+dropout_taxe_group = []
+use_taxe_group = []
+expected_number_of_elements_group = []
 
 class Simulator:
     def __init__(self, queue_size, lamb, lamb_server):
@@ -67,11 +73,15 @@ class Simulator:
             w += element.exit - element.arrival
         print(colored("| Média de tempo na fila: " + str(w/len(self.processed_queue)), "magenta"))
         print("-------------------------------------------")
+
+        average_time_group.append(w/len(self.processed_queue))
         return(w/len(self.processed_queue))
 
     def dropout_taxe(self):
         print (colored("| Taxa de desistência: " + str(float(self.num_dropouts)/self.num_arrivals), "yellow"))
         print("-------------------------------------------")
+
+        dropout_taxe_group.append(float(self.num_dropouts)/self.num_arrivals)
         return float(self.num_dropouts)/self.num_arrivals
 
     def use_taxe(self):
@@ -80,6 +90,8 @@ class Simulator:
             w += element.exit - element.start_processing
         print(colored("| Taxa de utilização: " + str(w/self.simulation_time), "cyan"))
         print("-------------------------------------------")
+
+        use_taxe_group.append(w/self.simulation_time)
         return (w/self.simulation_time)
 
 
@@ -92,15 +104,59 @@ class Simulator:
         expected_number = integral/self.simulation_time
         print(colored("| Número Esperado de Elementos na Fila: " + str(expected_number), "blue"))
         print("-------------------------------------------")
+
+        expected_number_of_elements_group.append(expected_number)
         return expected_number
 
+def title():
+    print("##############################################################")
+    print("#####                                                    #####")
+    print("#####        SIMULADOR DE AVALIAÇÃO DE DESEMPENHO        #####")
+    print("#####                       GRUPO:                       #####")
+    print("#####   LUAN SIMÕES, MATHEUS MAROTTI E THIAGO DO PRADO   #####")
+    print("#####                                                    #####")
+    print("##############################################################")
+    print()
+
+def breakLine():
+    print()
+    print("###################################################")
+    print()
+
+def testGenerator(queue_size, lamb, lamb_server, simulation_time):
+    teste = Simulator(queue_size, lamb, lamb_server)
+    inicial = Arrival(0)
+    teste.simulate(inicial, simulation_time)
+    teste.average_time()
+    teste.dropout_taxe()
+    teste.use_taxe()
+    teste.expected_number_of_elements()
+
+def examples():
+    title()
+    
+    for i in range(0,16):
+        testGenerator(15, 0.11, 0.09, 3600)
+        breakLine()
+
+    plt.plot( average_time_group )
+    plt.title("Média de tempo na fila")
+    plt.show()
+
+    plt.plot( dropout_taxe_group )
+    plt.title("Taxa de desistência")
+    plt.show()
+
+    plt.plot( use_taxe_group )
+    plt.title("Taxa de utilização")
+    plt.show()
+
+    plt.plot( expected_number_of_elements_group )
+    plt.title("Número esperado de elementos na fila")
+    plt.show()
+
+examples()
 
 
 
-teste = Simulator(15, 60, 20)
-inicial = Arrival(0)
-teste.simulate(inicial, 20)
-teste.average_time()
-teste.dropout_taxe()
-teste.use_taxe()
-teste.expected_number_of_elements()
+
